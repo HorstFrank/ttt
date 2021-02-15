a8dc26507b68e13951a7216420ceb1db31a7b10e
HorstFrank <boris.baumann@gmail.com>
2021-02-15 16:53:41 +0100

    add propstype

--- components/Square.tsx
+++ components/Square.tsx
@@ -1,9 +1,18 @@
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
 import styles from "../styles/Square.module.css";
+// import { useState } from "react";
 
 type Props = {
   value: ReactNode;
+  onClick?(): void;
 };
+
 export default function Square(props: Props) {
-  return <button className={styles.square}>{props.value}</button>;
+  // const [valueF, setValueXXX] = useState(null);
+
+  return (
+    <button className={styles.square} onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
